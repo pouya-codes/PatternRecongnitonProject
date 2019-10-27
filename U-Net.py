535ab808827540fdcdddffb5f460122e38b81083
pouya1991 <pouya.ahmadvand@gmail.com>
2019-10-27 22:22:34 +0330

    Pure U-net Implimentation

---
+++ U-Net.py
@@ -0,0 +1,104 @@
+from PIL import Image
+import cv2
+import numpy as np
+import stack_wrapper
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+
+# load train Data
+Train_X = np.zeros((30,512,512),dtype=np.int16)
+Train_Y = np.zeros((30,512,512),dtype=np.int16)
+sw_data  = stack_wrapper.Stack_wrapper('Data/train-volume.tif')
+sw_lable = stack_wrapper.Stack_wrapper('Data/train-labels.tif')
+
+
+for i in range(30) :
+    Train_X[i] = sw_data.next()
+    Train_Y[i] = sw_lable.next()
+
+# normalizing data
+Train_X = Train_X/255.0
+Train_X = np.expand_dims(Train_X,axis=-1)
+Train_Y = Train_Y/255.0
+Train_Y = np.expand_dims(Train_Y,axis=-1)
+
+
+
+
+def down_block(x, filters, kernel_size=(3, 3), padding="same", strides=1):
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    p = keras.layers.MaxPool2D((2, 2), (2, 2))(c)
+    return c, p
+
+def up_block(x, skip, filters, kernel_size=(3, 3), padding="same", strides=1):
+    us = keras.layers.UpSampling2D((2, 2))(x)
+    concat = keras.layers.Concatenate()([us, skip])
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(concat)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    return c
+
+def bottleneck(x, filters, kernel_size=(3, 3), padding="same", strides=1):
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    return c
+
+
+def UNet():
+    filters = [16, 32, 64, 128, 256]
+    inputs = keras.layers.Input((512, 512, 1))
+
+    p0 = inputs
+    c1, p1 = down_block(p0, filters[0])  # 128 -> 64
+    c2, p2 = down_block(p1, filters[1])  # 64 -> 32
+    c3, p3 = down_block(p2, filters[2])  # 32 -> 16
+    c4, p4 = down_block(p3, filters[3])  # 16->8
+
+    bn = bottleneck(p4, filters[4])
+
+    u1 = up_block(bn, c4, filters[3])  # 8 -> 16
+    u2 = up_block(u1, c3, filters[2])  # 16 -> 32
+    u3 = up_block(u2, c2, filters[1])  # 32 -> 64
+    u4 = up_block(u3, c1, filters[0])  # 64 -> 128
+
+    outputs = keras.layers.Conv2D(1, (1, 1), padding="same", activation="sigmoid")(u4)
+    model = keras.models.Model(inputs, outputs)
+    return model
+
+
+model = UNet()
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
+model.summary()
+model.fit(Train_X,Train_Y,validation_split=0.2,epochs=50,batch_size=1)
+
+
+model.save_weights("UNetWeights.h5")
+
+# load test Data
+Test_X = np.zeros((30,512,512),dtype=np.int16)
+sw_data  = stack_wrapper.Stack_wrapper('Data/test-volume.tif')
+for i in range(30) :
+    Test_X[i] = sw_data.next()
+
+Test_X = Test_X/255.0
+Test_X = np.expand_dims(Test_X,axis=-1)
+
+result = model.predict(Test_X)
+
+result = result > 0.5
+
+np.save('result.npy',result)
+
+fig = plt.figure()
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+ax = fig.add_subplot(1, 2, 1)
+ax.imshow(np.reshape(Test_X[0]*255, (512, 512)), cmap="gray")
+
+ax = fig.add_subplot(1, 2, 2)
+ax.imshow(np.reshape(result[0]*255, (512, 512)), cmap="gray")
+
+plt.show()
+
---
+++ stack_wrapper.py
@@ -0,0 +1,39 @@
+import PIL.Image
+import numpy as np
+class Stack_wrapper(object):
+    def __init__(self,fname):
+        '''fname is the full path '''
+        self.im  = PIL.Image.open(fname)
+
+        self.im.seek(0)
+        # get image dimensions from the meta data the order is flipped
+        # due to row major v col major ordering in tiffs and numpy
+        self.im_sz = [self.im.tag[0x101][0],
+                      self.im.tag[0x100][0]]
+        self.cur = self.im.tell()
+
+    def get_frame(self,j):
+        '''Extracts the jth frame from the image sequence.
+        if the frame does not exist return None'''
+        try:
+            self.im.seek(j)
+        except EOFError:
+            return None
+
+        self.cur = self.im.tell()
+        return np.reshape(self.im.getdata(),self.im_sz)
+    def __iter__(self):
+        self.im.seek(0)
+        self.old = self.cur
+        self.cur = self.im.tell()
+        return self
+
+    def next(self):
+        try:
+            self.im.seek(self.cur)
+            self.cur = self.im.tell()+1
+        except EOFError:
+            self.im.seek(self.old)
+            self.cur = self.im.tell()
+            raise StopIteration
+        return np.reshape(self.im.getdata(),self.im_sz)
